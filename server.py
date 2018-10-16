c03d80888f669ea80898720efec8190b21e360be
Leon Goldberg <lgoldber@redhat.com>
2018-10-16 15:08:51 +0300

    adding caching; minor error handling

--- server.py
+++ server.py
@@ -13,22 +13,34 @@ from moovit import get_routes
 app = Flask(__name__)
 CORS(app)
 
+cache = {}
 
 @app.route('/query')
 def main():
+    global cache
+
     address_1 = request.args.get('address_1')
     address_2 = request.args.get('address_2')
     if not address_1 or not address_2:
-        print(address_1, address_2)
-        return
+        return "Requires address_1 and address_2 parameters."
+
+    cached_result = cache.get((address_1, address_2))
+    if cached_result:
+        return cached_result
+
     proc = subprocess.Popen(
         [os.path.join(os.path.dirname(os.path.realpath(__file__)), 'moovit.py'),
          address_1, address_2],
         stdout=subprocess.PIPE, stderr=subprocess.STDOUT
     )
     result = proc.communicate()[0].decode('utf-8').strip()
-    print("response:", json.dumps(result))
-    return json.dumps(result)
+    try:
+        json.loads(result)
+    except ValueError:
+        result = [["no data", []]]
+    result = json.dumps(result)
+    cache[(address_1, address_2)] = result
+    return result
 
 if __name__ == '__main__':
     app.run()
