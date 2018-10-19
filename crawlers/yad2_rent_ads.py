2efbd338d7d8e5c45baa4f884c2640f6486b41ce
Leon Goldberg <lgoldber@redhat.com>
2018-10-19 11:16:59 +0300

    crawler improvemneets

--- crawlers/yad2_rent_ads.py
+++ crawlers/yad2_rent_ads.py
@@ -13,6 +13,8 @@ from pyquery import PyQuery as pq
 from common import get_config
 from common import setup_logging
 
+cache = set()
+
 
 def _get_location(item):
     location = item.find('td')[8].text
@@ -20,29 +22,35 @@ def _get_location(item):
 
 
 def crawl(driver, url, dst_location, params):
+    global cache
+
     logging.info('crawling...')
     yad2_url = 'http://www.yad2.co.il/Nadlan/rent.php?' + params
+    for page in range(1, 5):
+        yad2_url += '&Page=' + page
+        driver.get(yad2_url)
+        driver.implicitly_wait(10)
 
-    driver.get(yad2_url)
-    driver.implicitly_wait(10)
+        document = pq(driver.page_source)
+        main_table = document.find("div[id=main_table]")
 
-    document = pq(driver.page_source)
-    main_table = document.find("div[id=main_table]")
+        items = pq(main_table).find("tr[data-feed-place]")
+        if not items:
+            logging.warning('no locations, yad2 url: ' + yad2_url)
+            continue
 
-    items = pq(main_table).find("tr[data-feed-place]")
-    if not items:
-        logging.warning('no locations, yad2 url: ' + yad2_url)
-        return
+        src_locations = [_get_location(pq(item)) for item in items]
+        logging.debug('src_locations: {}'.format(src_locations))
 
-    src_locations = [_get_location(pq(item)) for item in items]
-    logging.debug('src_locations: {}'.format(src_locations))
-
-    for src_location in src_locations:
-        requests.post(
-            url,
-            data={'dst_addr': dst_location, 'src_addr': src_location}
-        )
-        time.sleep(20)
+        for src_location in src_locations:
+            if src_location in cache:
+                continue
+            cache.add(src_location)
+            requests.post(
+                url,
+                data={'dst_addr': dst_location, 'src_addr': src_location}
+            )
+            time.sleep(20)
 
 
 if __name__ == '__main__':
