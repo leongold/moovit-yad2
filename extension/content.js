428426e34cb133166524857a1ff9df361a05fed1
Leon Goldberg <lgoldber@redhat.com>
2018-10-20 20:08:18 +0300

    improved extension

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,4 @@
 
-var url;
-var address_2;
-
 function create_route_strings(data) {
     var strings = [];
     for (var i = 0; i < data.length; i++) {
@@ -32,8 +29,14 @@ function create_route_strings(data) {
 }
 
 function onSuccess(data, contact_info, addr_1, addr_2) {
+    if ("error" in data) {
+        $(contact_info).append(
+            "<div>" + data["error"] + "</div>"
+        )
+        return;
+    }
     var strings = create_route_strings(data["routes"]);
-    var ele = "<div>" + addr_1 + ' -> ' + addr_2 + ' ( ' + data["date"] + ')<br>'
+    var ele = "<div>" + data["date"] + "<br>";
     for (var i = 0; i < strings.length; i++) {
         ele += strings[i] + '<br>'
     }
@@ -58,38 +61,41 @@ var observer = new MutationObserver(function(mutations) {
                 var iframe = $(node).find("iframe")[0];
                 var iframe_document = iframe.contentWindow.document
                 var details = $(iframe_document).find("div.details_block_body")[0]
+                var details_items = $(details).find("div tr")
                 var contact_info = $(iframe_document).find("div.details_block_body")[2]
-                var city = $(details).find("div tr td")[3].innerText.trim()
-                var street = $(details).find("div tr td")[9].innerText.split(" ").slice(0, -1).join(" ").trim()
+                for (i=0; i < details_items.length-1; i++) {
+                    var items = $(details_items[i]).find("td");
+                    var key = items[0].innerText;
+                    if (key.includes("ישוב")) {
+                        var city = items[1].innerText.trim();
+                    }
+                    else if (key.includes("כתובת")) {
+                        var street = items[1].innerText.trim();
+                    }
+                }
                 var address_1 = street + ',' + city;
+                var address_2 = $(document).find("input[id=dst]").val() || $(document).find("input[id=dst]").attr("placeholder");
                 console.log("requiring routes:", address_1, address_2)
                 $.ajax({
                         type: 'GET',
-                        url: url,
+                        url: $(document).find("input[id=url]").val() || $(document).find("input[id=url]").attr("placeholder"),
                         data: {
                             'src_addr': address_1,
-                            'dst_addr': address_2
+                            'dst_addr': address_2,
                         },
                         dataType: "json",
                         success: function(response) {
                             onSuccess(response, contact_info, address_1, address_2);
                         },
                         error: function(request, status, error) {
-                            console.log(request, status. errror);
+                            console.log(request, status, error);
                         }
                 });
+                $(contact_info).append("<div>טוען מסעות עבור " + address_1 + " -> " + address_2 + "...</div>");
             }, 2500);
         }
     });
 });
-
-fetch(chrome.runtime.getURL("config"))
-    .then(function(response) {
-        return response.json().then(function(json) {
-            address_2 = json["location"],
-            url = json["url"]
-        }) 
-    })
 
 var config = {
     attributes: false,
@@ -99,3 +105,25 @@ var config = {
 };
 
 observer.observe(document.body, config);
+
+var tbl = $($(document).find("div.mainTable_top_right td[align=left] tr")[0])
+tbl.append($('<td><form>url<input type="text" placeholder="http://localhost:5000" id="url"></form></td>'))
+tbl.append($('<td><form>dst<input type="text" placeholder="דרך ירושליים 34,רעננה" id="dst"></form></td>'))
+
+var url = $(document).find("input[id=url]")
+$(url).on("input", function() { 
+    var value = $(document).find("input[id=url]").val();
+    chrome.storage.local.set({ "url-input": value }, function () {});
+});
+chrome.storage.local.get(["url-input"], function (result) {
+    $(url).val(result["url-input"] || $(url).attr("placeholder"))
+});
+
+dst = $(document).find("input[id=dst]")
+$(dst).on("input", function () {
+    var value = $(document).find("input[id=dst]").val();
+    chrome.storage.local.set({ "dst-input": value }, function () {});
+});
+chrome.storage.local.get(["dst-input"], function (result) {
+    $(dst).val(result["dst-input"] || $(dst).attr("placeholder"))
+});
